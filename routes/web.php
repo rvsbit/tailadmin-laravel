d99b18c96ba9bc00b7bf1775d82b6cc24c3bbc7a
itrvsb-developer <itrvsb19@gmail.com>
2026-05-05 11:46:40 +0800

    remove unnecessary routes

--- routes/web.php
+++ routes/web.php
@@ -9,12 +9,12 @@ use App\Http\Controllers\SalesController;
 use App\Http\Controllers\UserController;
 
 // authentication routes
-Route::get('/', fn() => redirect()->route('signin'));
-// Route::middleware('guest.session')->group(function () {
+// Route::get('/', fn() => redirect()->route('signin'));
+Route::middleware('guest.session')->group(function () {
     Route::get('/signin', fn() => view('pages.auth.signin'))->name('signin');
     Route::post('/signin', [AuthController::class, 'login'])->name('signin.post');
     Route::get('/signup', fn() => view('pages.auth.signup'))->name('signup');
-// });
+});
 
 Route::middleware('web')->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
@@ -34,45 +34,7 @@ Route::middleware('web')->group(function () {
     Route::resource('users', UserController::class);
 });
 
-// calender pages
-Route::get('/calendar', function () {
-    return view('pages.calender', ['title' => 'Calendar']);
-})->name('calendar');
 
-// profile pages
-Route::get('/profile', function () {
-    return view('pages.profile', ['title' => 'Profile']);
-})->name('profile');
-
-// form pages
-Route::get('/form-elements', function () {
-    return view('pages.form.form-elements', ['title' => 'Form Elements']);
-})->name('form-elements');
-
-// tables pages
-Route::get('/basic-tables', function () {
-    return view('pages.tables.basic-tables', ['title' => 'Basic Tables']);
-})->name('basic-tables');
-
-// pages
-
-Route::get('/blank', function () {
-    return view('pages.blank', ['title' => 'Blank']);
-})->name('blank');
-
-// error pages
-Route::get('/error-404', function () {
-    return view('pages.errors.error-404', ['title' => 'Error 404']);
-})->name('error-404');
-
-// chart pages
-Route::get('/line-chart', function () {
-    return view('pages.chart.line-chart', ['title' => 'Line Chart']);
-})->name('line-chart');
-
-Route::get('/bar-chart', function () {
-    return view('pages.chart.bar-chart', ['title' => 'Bar Chart']);
-})->name('bar-chart');
 
 
 // authentication pages
@@ -85,31 +47,6 @@ Route::get('/signin', function () {
 Route::get('/signup', function () {
     return view('pages.auth.signup', ['title' => 'Sign Up']);
 })->name('signup');
-
-// ui elements pages
-Route::get('/alerts', function () {
-    return view('pages.ui-elements.alerts', ['title' => 'Alerts']);
-})->name('alerts');
-
-Route::get('/avatars', function () {
-    return view('pages.ui-elements.avatars', ['title' => 'Avatars']);
-})->name('avatars');
-
-Route::get('/badge', function () {
-    return view('pages.ui-elements.badges', ['title' => 'Badges']);
-})->name('badges');
-
-Route::get('/buttons', function () {
-    return view('pages.ui-elements.buttons', ['title' => 'Buttons']);
-})->name('buttons');
-
-Route::get('/image', function () {
-    return view('pages.ui-elements.images', ['title' => 'Images']);
-})->name('images');
-
-Route::get('/videos', function () {
-    return view('pages.ui-elements.videos', ['title' => 'Videos']);
-})->name('videos');
 
 
 

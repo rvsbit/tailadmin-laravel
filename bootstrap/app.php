11c05703330098e4b3b3a4018c8c16014f937526
itrvsb-developer <itrvsb19@gmail.com>
2026-05-05 11:49:51 +0800

    set authenctication redirect if session expired

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,8 +11,19 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->alias([
+            'guest.session' => \App\Http\Middleware\RedirectIfAuthenticated::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->render(function (\Illuminate\Auth\AuthenticationException $e, Request $request) {
+            if ($request->expectsJson()) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Session expired. Please log in again.',
+                ], 401);
+            }
+
+            return redirect()->route('signin');
+        });
     })->create();
